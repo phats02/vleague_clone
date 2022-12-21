dd3e35ad1cf14ad6f39a1c7790119dbcf0524425
thach030103 <120116007+thach030103@users.noreply.github.com>
2022-12-21 16:10:25 +0700

    Update db.js

--- db/db.js
+++ db/db.js
@@ -1,22 +1,19 @@
 const fs = require('fs');
-
 let rawdata = fs.readFileSync('./db.json');
 let doibong = JSON.parse(rawdata);
 let dsDoiBongThamDu = doibong.Doi;
 var n= dsDoiBongThamDu.length/2;
 for (let i = 1 ; i < 2*n ; i++){
     let sovong=i;
-    for ( let j = 0; j < n ; j++){
-    
+    for ( let j = 0; j < n ; j++){ 
             if ( j==0 ){
                 home = i;   away = 2*n;
-                SoBanThangDoi1=Math.floor(Math.random() * 5);
-                SoBanThangDoi2=Math.floor(Math.random() * 5);
                 let trandau = {
                     VongDau: sovong,
                     Doi1:  dsDoiBongThamDu[home-1].TenDoi,
                     Doi2: dsDoiBongThamDu[away-1].TenDoi, 
-                    Tiso: SoBanThangDoi1 + ' - ' + SoBanThangDoi2
+                    SoBanThangDoi1: Math.floor(Math.random() * 5),
+                    SoBanThangDoi2: Math.floor(Math.random() * 5)
                 }
                 console.log(trandau);
             }
@@ -26,36 +23,30 @@ for (let i = 1 ; i < 2*n ; i++){
                 if(home ===0)	home = 2*n - 1
                 if(away >= 2*n)	away = away % (2*n - 1)
                 if(dsDoiBongThamDu[home-1].TenDoi != dsDoiBongThamDu[away-1].TenDoi)
-                {
-                    SoBanThangDoi1=Math.floor(Math.random() * 5);
-                    SoBanThangDoi2=Math.floor(Math.random() * 5);
+                {                
                     let trandau = {
                         VongDau: sovong,
                         Doi1:  dsDoiBongThamDu[home-1].TenDoi,
-                        Doi2: dsDoiBongThamDu[away-1].TenDoi, 
-                        Tiso: SoBanThangDoi1 + ' - ' + SoBanThangDoi2
+                        SoBanThangDoi1: Math.floor(Math.random() * 5),
+                        SoBanThangDoi2: Math.floor(Math.random() * 5)
                     }
                     console.log(trandau);
                 }
-            }
-                
+            }                
     }
     
 }
 for (let i = 1 ; i < 2*n ; i++){
     let sovong=i+2*n-1;
-    for ( let j = 0; j < n ; j++){
-        
+    for ( let j = 0; j < n ; j++){       
         if ( j==0 ){
-            home = 2*n;   away = i;
-            SoBanThangDoi1=Math.floor(Math.random() * 5);
-            SoBanThangDoi2=Math.floor(Math.random() * 5);
-            let trandau = {
-                     
+            home = 2*n;   away = i;            
+            let trandau = {                     
                 VongDau: sovong,
                 Doi1:  dsDoiBongThamDu[home-1].TenDoi,
                 Doi2: dsDoiBongThamDu[away-1].TenDoi, 
-                Tiso: SoBanThangDoi1 + ' - ' + SoBanThangDoi2
+                SoBanThangDoi1: Math.floor(Math.random() * 5),
+                SoBanThangDoi2: Math.floor(Math.random() * 5)
                 }
                 console.log(trandau);
         }
@@ -66,13 +57,12 @@ for (let i = 1 ; i < 2*n ; i++){
             if(home >= 2*n)	home = home % (2*n - 1)
             if(dsDoiBongThamDu[home-1].TenDoi != dsDoiBongThamDu[away-1].TenDoi)
             {
-                SoBanThangDoi1=Math.floor(Math.random() * 5);
-                SoBanThangDoi2=Math.floor(Math.random() * 5);
                 let trandau = {
                     VongDau: sovong,
                     Doi1:  dsDoiBongThamDu[home-1].TenDoi,
                     Doi2: dsDoiBongThamDu[away-1].TenDoi, 
-                    Tiso: SoBanThangDoi1 + ' - ' + SoBanThangDoi2
+                    SoBanThangDoi1: Math.floor(Math.random() * 5),
+                    SoBanThangDoi2: Math.floor(Math.random() * 5)
                     }
                     console.log(trandau);
             }
