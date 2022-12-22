04028781d04e087ce865c4b7933afdd62486489a
20120151 <nguyenthanhnhan24092002@gmail.com>
2022-12-22 18:00:07 +0700

    update config

--- config/pg-config.js
+++ config/pg-config.js
@@ -1,17 +1,17 @@
-module.exports = {
-    host: 'localhost',
-    port: 5432,
-    database: 'QuanLyBongDa',
-    user: 'postgres',
-    password: 'quangphat',
-    max: 30 // use up to 30 connections
-};
-
-// module.exports={
+// module.exports = {
 //     host: 'localhost',
 //     port: 5432,
-//     database:'data1',
-//     user:'postgres',
-//     password:'1234567890',
-//     max: 30
-// }
+//     database: 'QuanLyBongDa',
+//     user: 'postgres',
+//     password: 'quangphat',
+//     max: 30 // use up to 30 connections
+// };
+
+module.exports={
+    host: 'localhost',
+    port: 5432,
+    database:'data1',
+    user:'postgres',
+    password:'1234567890',
+    max: 30
+}
