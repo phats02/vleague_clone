f3ad4dce8844c0618b1533e61f9c4d69bc0433b2
20120367 <hokhanhtam22@gmail.com>
2022-12-22 23:40:11 +0700

    edit admin result page

--- controllers/admin.c.js
+++ controllers/admin.c.js
@@ -18,7 +18,7 @@ exports.menu = (req, res, next) => {
     }
 }
 exports.mathResult = async (req, res, next) => {
-    //sửa cái này nè đmm
+    //sửa cái này nè má mệt vãi 
     var idMatch = req.params.id
     if (req.method == 'GET') {
         var page = req.query.page
