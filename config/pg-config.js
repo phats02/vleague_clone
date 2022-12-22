808024a2530150b52e6b3e1901b92de9a25d5b7b
20120151 <nguyenthanhnhan24092002@gmail.com>
2022-12-22 14:09:49 +0700

    fix config to test

--- config/pg-config.js
+++ config/pg-config.js
@@ -1,8 +1,17 @@
-module.exports = {
+// module.exports = {
+//     host: 'localhost',
+//     port: 5432,
+//     database: 'QuanLyBongDa',
+//     user: 'postgres',
+//     password: 'quangphat',
+//     max: 30 // use up to 30 connections
+// };
+
+module.exports={
     host: 'localhost',
     port: 5432,
-    database: 'QuanLyBongDa',
-    user: 'postgres',
-    password: 'quangphat',
-    max: 30 // use up to 30 connections
-};
+    database:'main_CNPM',
+    user:'postgres',
+    password:'1234567890',
+    max: 30
+}
