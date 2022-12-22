680c70ad663e93a93e464b8f29ea07321b1f1262
20120151 <nguyenthanhnhan24092002@gmail.com>
2022-12-22 09:30:41 +0700

    update db.js to test

--- models/db.js
+++ models/db.js
@@ -51,26 +51,26 @@ module.exports={
         for(var i=0;i<data.San.length;i++){
             var a=data.San[i].TenSan;
             //console.log(a);
-            //db.one('INSERT INTO "SAN"("TenSan") VALUES($1) ON CONFLICT DO NOTHING RETURNING $1', [data.San[i].TenSan]);
+            db.one('INSERT INTO "SAN"("TenSan") VALUES($1) ON CONFLICT DO NOTHING RETURNING $1', [data.San[i].TenSan]);
         }
         dataLoaiBanThang=data.LoaiBanThang;
         for(var i=0;i<dataLoaiBanThang.length;i++){
             var a=data.LoaiBanThang[i].TenLoaiBanThang;
             //console.log(a);
-            //db.one('INSERT INTO "LOAIBANTHANG"("TenLoaiBanThang") VALUES($1) ON CONFLICT DO NOTHING RETURNING $1', [data.LoaiBanThang[i].TenLoaiBanThang]);
+            db.one('INSERT INTO "LOAIBANTHANG"("TenLoaiBanThang") VALUES($1) ON CONFLICT DO NOTHING RETURNING $1', [data.LoaiBanThang[i].TenLoaiBanThang]);
         }
         dataDoi=data.Doi;
         for(var i=0;i<data.Doi.length;i++){
             var a=data.Doi[i];
             //console.log(a.TenDoi);
-            //db.one('INSERT INTO "DOI"("TenDoi","MaSan","SoCauThu") VALUES($1,$2,$3) ON CONFLICT DO NOTHING RETURNING $1', [data.Doi[i].TenDoi,data.Doi[i].MaSan,data.Doi[i].SoCauThu]);
+            db.one('INSERT INTO "DOI"("TenDoi","MaSan","SoCauThu") VALUES($1,$2,$3) ON CONFLICT DO NOTHING RETURNING $1', [data.Doi[i].TenDoi,data.Doi[i].MaSan,data.Doi[i].SoCauThu]);
         }
         dataCauThu=data.CauThu;
         for(var i=0;i<14;i++){
             for(var j=0;j<15;j++){
                 var a=data.CauThu[i][j].TenCauThu;
                 //console.log(a);
-                //db.one('INSERT INTO "CAUTHU"("TenCauThu","MaLoaiCauThu","MaDoi","GhiBan") VALUES($1,$2,$3,$4) ON CONFLICT DO NOTHING RETURNING $1', [data.CauThu[i][j].TenCauThu,data.CauThu[i][j].NgaySinh,data.CauThu[i][j].MaLoaiCauThu,data.CauThu[i][j].MaDoi,data.CauThu[i][j].GhiBan]);
+                db.one('INSERT INTO "CAUTHU"("TenCauThu","MaLoaiCauThu","MaDoi","GhiBan") VALUES($1,$2,$3,$4) ON CONFLICT DO NOTHING RETURNING $1', [data.CauThu[i][j].TenCauThu,data.CauThu[i][j].NgaySinh,data.CauThu[i][j].MaLoaiCauThu,data.CauThu[i][j].MaDoi,data.CauThu[i][j].GhiBan]);
             }
         }
 
