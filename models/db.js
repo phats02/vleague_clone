9024d0cbbfb22329d6065c7ead164d8c277e1630
20120151 <nguyenthanhnhan24092002@gmail.com>
2022-12-22 16:14:24 +0700

    update get data from json

--- models/db.js
+++ models/db.js
@@ -49,19 +49,19 @@ module.exports={
         const data=await readDataFromJson(pathJSON);
         dataSan=data.San;
         for(var i=0;i<data.San.length;i++){
-            //db.one('INSERT INTO "SAN"("TenSan") VALUES($1) ON CONFLICT DO NOTHING RETURNING $1', [data.San[i].TenSan]);
+            await db.one('INSERT INTO "SAN"("TenSan") VALUES($1) ON CONFLICT DO NOTHING RETURNING $1', [data.San[i].TenSan]);
         }
         dataLoaiBanThang=data.LoaiBanThang;
         for(var i=0;i<dataLoaiBanThang.length;i++){
-            //db.one('INSERT INTO "LOAIBANTHANG"("TenLoaiBanThang") VALUES($1) ON CONFLICT DO NOTHING RETURNING $1', [data.LoaiBanThang[i].TenLoaiBanThang]);
+            await db.one('INSERT INTO "LOAIBANTHANG"("TenLoaiBanThang") VALUES($1) ON CONFLICT DO NOTHING RETURNING $1', [data.LoaiBanThang[i].TenLoaiBanThang]);
         }
         dataDoi=data.Doi;
         for(var i=0;i<data.Doi.length;i++){
-            //db.one('INSERT INTO "DOI"("TenDoi","MaSan","SoCauThu") VALUES($1,$2,$3) ON CONFLICT DO NOTHING RETURNING $1', [data.Doi[i].TenDoi,data.Doi[i].MaSan,data.Doi[i].SoCauThu]);
+            await db.one('INSERT INTO "DOI"("TenDoi","MaSan","SoCauThu") VALUES($1,$2,$3) ON CONFLICT DO NOTHING RETURNING $1', [data.Doi[i].TenDoi,data.Doi[i].MaSan,data.Doi[i].SoCauThu]);
         }
         data.LoaiCauThu=data.LoaiCauThu;
         for(var i=0;i<data.LoaiCauThu.length;i++){
-            //db.one('INSERT INTO "LOAICAUTHU" ("TenLoaiCauThu") VALUES($1) ON CONFLICT DO NOTHING RETURNING $1', [data.LoaiCauThu[i].TenLoaiCauThu]);
+            await db.one('INSERT INTO "LOAICAUTHU" ("TenLoaiCauThu") VALUES($1) ON CONFLICT DO NOTHING RETURNING $1', [data.LoaiCauThu[i].TenLoaiCauThu]);
         }
         dataCauThu=data.CauThu;
         for(var i=0;i<14;i++){
@@ -70,7 +70,7 @@ module.exports={
                 const dateArray = a.split('/')
                 const [day, month, year] = dateArray;
                 const newDateString = `${month}/${day}/${year}`;
-                //db.one('INSERT INTO "CAUTHU"("TenCauThu","NgaySinh","MaLoaiCauThu","MaDoi","GhiBan") VALUES($1,$2,$3,$4,$5) ON CONFLICT DO NOTHING RETURNING $1', [data.CauThu[i][j].TenCauThu,newDateString,data.CauThu[i][j].MaLoaiCauThu,data.CauThu[i][j].MaDoi,data.CauThu[i][j].GhiBan]);
+                await db.one('INSERT INTO "CAUTHU"("TenCauThu","NgaySinh","MaLoaiCauThu","MaDoi","GhiBan") VALUES($1,$2,$3,$4,$5) ON CONFLICT DO NOTHING RETURNING $1', [data.CauThu[i][j].TenCauThu,newDateString,data.CauThu[i][j].MaLoaiCauThu,data.CauThu[i][j].MaDoi,data.CauThu[i][j].GhiBan]);
             }
         }
         dataTranDau=data.Trandau;
@@ -82,7 +82,7 @@ module.exports={
             const newDateString = `${month}-${day}-${year}`;
             const newDatetimeString = `${newDateString} ${timeString}`;
             //console.log(newDatetimeString);
-            //db.one('INSERT INTO "TRANDAU"("MaDoi1","MaDoi2","NgayGio","MaSan","VongDau","SoBanThangDoi1","SoBanThangDoi2") VALUES($1,$2,$3,$4,$5,$6,$7) ON CONFLICT DO NOTHING RETURNING $1', [data.Trandau[i].MaDoi1,data.Trandau[i].MaDoi2,newDatetimeString,data.Trandau[i].MaSan,data.Trandau[i].VongDau,data.Trandau[i].SoBanThangDoi1,data.Trandau[i].SoBanThangDoi2]);
+            await db.one('INSERT INTO "TRANDAU"("MaDoi1","MaDoi2","NgayGio","MaSan","VongDau","SoBanThangDoi1","SoBanThangDoi2") VALUES($1,$2,$3,$4,$5,$6,$7) ON CONFLICT DO NOTHING RETURNING $1', [data.Trandau[i].MaDoi1,data.Trandau[i].MaDoi2,newDatetimeString,data.Trandau[i].MaSan,data.Trandau[i].VongDau,data.Trandau[i].SoBanThangDoi1,data.Trandau[i].SoBanThangDoi2]);
         }
     }
 
