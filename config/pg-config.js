574696ca1139aa13e65ba827c79cdb45a808f700
20120367 <hokhanhtam22@gmail.com>
2022-12-23 14:53:40 +0700

    edit some pages and edit db.json

--- config/pg-config.js
+++ config/pg-config.js
@@ -1,19 +1,19 @@
+// module.exports = {
+//     host: 'localhost',
+//     port: 5432,
+//     database: 'QuanLyBongDa',
+//     user: 'postgres',
+//     password: 'quangphat',
+//     max: 30 // use up to 30 connections
+// };
 module.exports = {
     host: 'localhost',
     port: 5432,
     database: 'QuanLyBongDa',
     user: 'postgres',
-    password: 'quangphat',
+    password: '010203',
     max: 30 // use up to 30 connections
 };
-// module.exports = {
-//     host: 'localhost',
-//     port: 5432,
-//     database: 'QLBD5',
-//     user: 'postgres',
-//     password: '010203',
-//     max: 30 // use up to 30 connections
-// };
 // module.exports={
 //     host: 'localhost',
 //     port: 5432,
